885b1f3075a35a0c508f088403db2b79632676cc
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-11-20 15:22:24 +0000

    fix the indentation

--- examples/basic-proof/tests/my_example_test.py
+++ examples/basic-proof/tests/my_example_test.py
@@ -25,17 +25,17 @@ class TestE2E(unittest.TestCase):
         print (self.result)
         self.assertEqual(self.result["destroy"], False)
 
-        def test_instance_type(self):
-            self.assertEqual(self.result["aws_instance.foo"]["instance_type"], "t2.micro")
+    def test_instance_type(self):
+        self.assertEqual(self.result["aws_instance.foo"]["instance_type"], "t2.micro")
 
-        def test_ami(self):
-            self.assertEqual(self.result["aws_instance.foo"]["ami"], "foo")
+    def test_ami(self):
+        self.assertEqual(self.result["aws_instance.foo"]["ami"], "foo")
 
-        def test_destroy(self):
-            self.assertEqual(self.result["aws_instance.foo"]["destroy"], False)
+    def test_destroy(self):
+        self.assertEqual(self.result["aws_instance.foo"]["destroy"], False)
 
-        def test_destroy_tainted(self):
-            self.assertEqual(self.result["aws_instance.foo"]["destroy_tainted"], False)
+    def test_destroy_tainted(self):
+        self.assertEqual(self.result["aws_instance.foo"]["destroy_tainted"], False)
 
 
 if __name__ == '__main__':
